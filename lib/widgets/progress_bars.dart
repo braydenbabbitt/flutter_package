cd117185542d72bccebf41d2661603d1645f0c5b
braydentbabbitt <brayden@babbitts.net>
2021-12-14 22:25:48 -0700

    Fixed text centering issue and finalized AnimatedRectProgressBar 1.0

--- lib/widgets/progress_bars.dart
+++ lib/widgets/progress_bars.dart
@@ -3,197 +3,9 @@ import 'dart:ui';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class AnimatedLinearProgressBar extends StatefulWidget {
-  final double value;
-  final double height;
-  final Color fillColor;
-  final Color backgroundColor;
-  final Color borderColor;
-  final double borderWidth;
-  final double outerCornerRadius;
-  final double? innerCornerRadius;
-  final String displayText;
-  final TextStyle? textStyle;
-
-  const AnimatedLinearProgressBar({
-    Key? key,
-    this.value = 0.5,
-    this.height = 50,
-    this.fillColor = Colors.black26,
-    this.backgroundColor = Colors.transparent,
-    this.borderColor = Colors.black,
-    this.borderWidth = 1.5,
-    this.outerCornerRadius = 10,
-    this.innerCornerRadius,
-    this.displayText = "",
-    this.textStyle,
-  }) : super(key: key);
-
-  @override
-  State<AnimatedLinearProgressBar> createState() => _AnimatedLinearProgressBarState();
-}
-
-class _AnimatedLinearProgressBarState extends State<AnimatedLinearProgressBar> with
-    SingleTickerProviderStateMixin {
-  late AnimationController _controller;
-  late CurvedAnimation _curvedAnimation;
-  late Tween<double> valueTween;
-
-  @override
-  void initState() {
-    super.initState();
-
-    valueTween = Tween<double>(
-      begin: widget.value,
-      end: widget.value,
-    );
-
-    _controller = AnimationController(
-      value: widget.value,
-      duration: const Duration(milliseconds: 500),
-      vsync: this,
-    );
-    _controller.forward();
-
-    _curvedAnimation = CurvedAnimation(
-      parent: _controller,
-      curve: Curves.easeInOut,
-    );
-  }
-
-  @override
-  void dispose() {
-    _controller.dispose();
-    _curvedAnimation.dispose();
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return AnimatedBuilder(
-      animation: _curvedAnimation,
-      child: SizedBox(
-        height: widget.height,
-        child: Center(
-          child: Text(
-            widget.displayText,
-            style: widget.textStyle ?? TextStyle(
-              fontSize: widget.height * 0.5,
-              color: widget.borderColor,
-              fontWeight: FontWeight.w600,
-            ),
-          ),
-        ),
-      ),
-      builder: (context, child) {
-        return CustomPaint(
-          child: child,
-          painter: AnimatedLinearProgressBarPainter(
-            percentage: valueTween.evaluate(_curvedAnimation),
-            height: widget.height,
-            color: widget.fillColor,
-            backgroundColor: widget.backgroundColor,
-            outerCornerRadius: widget.outerCornerRadius,
-            innerCornerRadius: (widget.innerCornerRadius != null) ? widget.innerCornerRadius : null,
-            paintingStyle: PaintingStyle.fill,
-          ),
-          foregroundPainter: AnimatedLinearProgressBarPainter(
-            height: widget.height,
-            color: widget.borderColor,
-            backgroundColor: widget.backgroundColor,
-            borderWidth: widget.borderWidth,
-            outerCornerRadius: widget.outerCornerRadius,
-            paintingStyle: PaintingStyle.stroke,
-          ),
-        );
-      },
-    );
-  }
-
-  @override
-  void didUpdateWidget(AnimatedLinearProgressBar oldWidget) {
-    super.didUpdateWidget(oldWidget);
-
-    _curvedAnimation = CurvedAnimation(
-      parent: _controller,
-      curve: Curves.easeInOut,
-    );
-    _controller.duration = Duration(milliseconds: (500).toInt());
-
-    if (widget.value != oldWidget.value) {
-      valueTween = Tween<double>(
-        begin: oldWidget.value,
-        end: widget.value,
-      );
-
-      _controller
-        ..value = 0
-        ..forward();
-    }
-  }
-}
-
-class AnimatedLinearProgressBarPainter extends CustomPainter {
-  final double? percentage;
-  final double height;
-  final Color color;
-  final Color? backgroundColor;
-  final PaintingStyle paintingStyle;
-  final double? borderWidth;
-  final double outerCornerRadius;
-  final double? innerCornerRadius;
-
-  AnimatedLinearProgressBarPainter({
-    this.percentage,
-    required this.height,
-    required this.color,
-    this.backgroundColor,
-    required this.paintingStyle,
-    this.borderWidth,
-    required this.outerCornerRadius,
-    this.innerCornerRadius,
-  });
-
-  @override
-  void paint(Canvas canvas, Size size) {
-    final paint = Paint()
-      ..color = color
-      ..strokeWidth = borderWidth ?? 0
-      ..strokeCap = StrokeCap.round
-      ..style = paintingStyle;
-    final double width = (percentage != null) ? (size.width * percentage!) : size.width;
-    final Radius rightRadius = (innerCornerRadius != null) ? Radius.circular(innerCornerRadius!) : Radius.circular(outerCornerRadius);
-
-    final clipRect = RRect.fromLTRBR(0, 0, size.width, size.height, Radius.circular(outerCornerRadius));
-    final bar = RRect.fromLTRBAndCorners(0, 0, width, height,
-      topLeft: Radius.circular(outerCornerRadius),
-      topRight: rightRadius,
-      bottomLeft: Radius.circular(outerCornerRadius),
-      bottomRight: rightRadius,
-    );
-
-    if (backgroundColor != null && backgroundColor != Colors.transparent) {
-      final backgroundPaint = Paint()
-        ..color = backgroundColor ?? Colors.transparent
-        ..style = PaintingStyle.fill;
-      canvas.drawRRect(bar, backgroundPaint);
-    }
-    if (percentage != null) {
-      canvas.clipRRect(clipRect);
-    }
-    canvas.drawRRect(bar, paint);
-    // canvas.drawRRect(fullBar, borderPaint);
-  }
-
-  @override
-  bool shouldRepaint(covariant CustomPainter oldDelegate) {
-    final old = (oldDelegate as _AnimatedRectProgressBarPainter);
-    return old.percentage != percentage;
-  }
-}
-
-// ========================================================================= //
-
+// AnimatedRectProgressBar V1.0
+// version ideas:
+//   V2.0 Add gradient dependent on percentage fill
 class AnimatedRectProgressBar extends StatefulWidget {
   //Required properties
   final double value;
@@ -276,52 +88,45 @@ class _AnimatedRectProgressBarState extends State<AnimatedRectProgressBar> with
 
   @override
   Widget build(BuildContext context) {
-    return AnimatedBuilder(
-      animation: _curvedAnimation,
-      child: SizedBox(
+    return SizedBox(
         height: widget.height,
         width: widget.width,
-        child: (widget.displayText != null) ? Center(
+        child: AnimatedBuilder(
+          animation: _curvedAnimation,
+          child: (widget.displayText != null) ? Center(
+            heightFactor: widget.height,
+            widthFactor: widget.width,
             child: Text(
               widget.displayText!,
               style: widget.textStyle,
-            )
-        ) : null,
-      ),
-      builder: (context, child) {
-        return CustomPaint(
-          child: (widget.displayText != null) ? SizedBox(
-            width: widget.width,
-            height: widget.height,
-            child: Center(
-                child: Text(
-                  widget.displayText!,
-                  style: widget.textStyle,
-                )
             ),
           ) : null,
-          painter: _AnimatedRectProgressBarPainter(
-            percentage: _valueTween.evaluate(_curvedAnimation),
-            color: widget.fillColor,
-            cornerRadius: widget.innerCornerRadius,
-            paintingStyle: PaintingStyle.fill,
-            backgroundColor: widget.backgroundColor,
-            fillDirection: widget.fillDirection,
-            height: widget.height,
-            width: widget.width,
-            clipCornerRadius: widget.outerCornerRadius,
-            borderWidth: widget.borderWidth,
-          ),
-          foregroundPainter: (widget.borderColor == null || widget.borderWidth == null) ? null : _AnimatedRectProgressBarPainter(
-            color: widget.borderColor!,
-            cornerRadius: widget.outerCornerRadius,
-            paintingStyle: PaintingStyle.stroke,
-            height: widget.height,
-            width: widget.width,
-            borderWidth: widget.borderWidth,
-          ),
-        );
-      },
+          builder: (context, child) {
+            return CustomPaint(
+              child: child,
+              painter: _AnimatedRectProgressBarPainter(
+                percentage: _valueTween.evaluate(_curvedAnimation),
+                color: widget.fillColor,
+                cornerRadius: widget.innerCornerRadius,
+                paintingStyle: PaintingStyle.fill,
+                backgroundColor: widget.backgroundColor,
+                fillDirection: widget.fillDirection,
+                height: widget.height,
+                width: widget.width,
+                clipCornerRadius: widget.outerCornerRadius,
+                borderWidth: widget.borderWidth,
+              ),
+              foregroundPainter: (widget.borderColor == null || widget.borderWidth == null) ? null : _AnimatedRectProgressBarPainter(
+                color: widget.borderColor!,
+                cornerRadius: widget.outerCornerRadius,
+                paintingStyle: PaintingStyle.stroke,
+                height: widget.height,
+                width: widget.width,
+                borderWidth: widget.borderWidth,
+              ),
+            );
+          },
+        )
     );
   }
 
